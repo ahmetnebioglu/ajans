242d275098f4466727457f789c49c7f3d9aa7724
Ahmet Nebioğlu <ahmetnebioglu89@gmail.com>
2026-04-30 22:56:19 +0300

    fix(teknikel): update auth strategy and cleanup settings page

--- apps/teknikel/auth.ts
+++ apps/teknikel/auth.ts
@@ -3,8 +3,5 @@ import { authOptions as commonAuthOptions } from "@ajans/auth";
 export const authOptions = {
   ...commonAuthOptions,
   secret: process.env.NEXTAUTH_SECRET,
-  adapter: undefined,
-  session: {
-    strategy: "jwt" as const,
-  },
+  session: { strategy: "jwt" },
 };
