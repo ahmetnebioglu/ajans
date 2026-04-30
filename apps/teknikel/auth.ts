58ad172e6b221f1578d50fc40250c95c119593ee
Ahmet Nebioğlu <ahmetnebioglu89@gmail.com>
2026-04-30 23:35:52 +0300

    fix: add NextAuthOptions type to authOptions in teknikel

--- apps/teknikel/auth.ts
+++ apps/teknikel/auth.ts
@@ -1,6 +1,7 @@
+import { NextAuthOptions } from "next-auth";
 import { authOptions as commonAuthOptions } from "@ajans/auth";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   ...commonAuthOptions,
   secret: process.env.NEXTAUTH_SECRET,
   session: { strategy: "jwt" },
