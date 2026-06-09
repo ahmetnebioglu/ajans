401a6b2e9da63af01da73ec4af870135dad0e8e7
Ahmet Nebioğlu <ahmetnebioglu89@gmail.com>
2026-06-09 12:27:40 +0300

    *

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,8 @@
 export default {
   schema: "./packages/db/prisma/schema.prisma",
-  datasource: {
-    url: process.env.DATABASE_URL
-  }
+  ...(process.env.DATABASE_URL ? {
+    datasource: {
+      url: process.env.DATABASE_URL
+    }
+  } : {})
 };
